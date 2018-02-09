72cdea5903ebd56386a12f0563b326407c7d6ff3
Emmita <emmanuelavilah@gmail.com>
2018-02-09 11:07:49 -0600

    Se completa CRUD

--- app/src/main/java/com/emmita/android/books/database/BookProvider.java
+++ app/src/main/java/com/emmita/android/books/database/BookProvider.java
@@ -23,15 +23,20 @@ public class BookProvider extends ContentProvider {
 
     public static final String LOG_TAG = BookProvider.class.getSimpleName();
 
+    //Código que nos indica si el contenido del URI coincide para la tabla books
     private static final int BOOKS = 100;
+    //Código que nos indica si el contenido del URI para un solo libro coincide in la tabla books
     private static final int BOOK_ID = 101;
 
     private BookDbHelper mBookDbHelper;
 
+    //UriMatcher nos ayuda a asegurarnos que el ContentProvider no está manejando contenido que no es
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     static{
+        //UriMatcher para la tabla completa
         sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS, BOOKS);
+        //UriMatcher para un elemento específico
         sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS + "/#", BOOK_ID);
 
     }
@@ -47,13 +52,54 @@ public class BookProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+
+        //Llama a getReadableDatabase() para poder consultar los elementos de la base de datos
+        SQLiteDatabase database = mBookDbHelper.getReadableDatabase();
+
+        //El cursor contiene el resultado de la consulta
+        Cursor cursor;
+
+        //Se asegura que el codigo del UriMatcher coincida con el URI dado
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case BOOKS:
+                //En caso de que el URI coincida con este código, la consulta traerá toda la información
+                //de la tabla books
+                cursor = database.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case BOOK_ID:
+                //En caso de que el URI coincida con este código, la consulta traerá la información
+                //solamente del ID especificado
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                cursor = database.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("No se puede consultar el URI desconocido " + uri);
+        }
+
+        //Se notifica al Cursor qué contenido tendrá
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case BOOKS:
+                return BookEntry.CONTENT_LIST_TYPE;
+            case BOOK_ID:
+                return BookEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("URI desconocido " + uri);
+        }
+
     }
 
     @Nullable
@@ -110,7 +156,36 @@ public class BookProvider extends ContentProvider {
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+
+        //Manda llamar a getWritableDatabase(), ya que se va a eliminar información
+        SQLiteDatabase database = mBookDbHelper.getWritableDatabase();
+
+        //Variable donde se almacenarán las filas que sean eliminadas
+        int rowsDeleted;
+
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case BOOKS:
+                //Elimina todas las filas que coincidan con la instruccion SQL
+                rowsDeleted = database.delete(BookEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case BOOK_ID:
+                //Elimina una unica fila, dada por el ID en el URI
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = database.delete(BookEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("La eliminación no es compatible con " + uri);
+        }
+
+        //Si una o mas filas han sido eliminadas, se notifica que la información del URI ha sido cambiada
+        if (rowsDeleted != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        //Regresa las filas eliminadas
+        return rowsDeleted;
     }
 
     @Override
@@ -121,6 +196,7 @@ public class BookProvider extends ContentProvider {
             case BOOKS:
                 return updateBook(uri, values, selection, selectionArgs);
             case BOOK_ID:
+                //Se conoce cuál fila será actualizada, puesto que el ID se extrae del URI
                 selection = BookEntry._ID + "=?";
                 selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
                 return updateBook(uri, values, selection, selectionArgs);
