cc34b2e7c8efd0ef3d2b1599153013623cacaf6e
Emmita <emmanuelavilah@gmail.com>
2018-02-09 02:02:11 -0600

    Se especifica el URI

--- app/src/main/java/com/emmita/android/books/database/BookContract.java
+++ app/src/main/java/com/emmita/android/books/database/BookContract.java
@@ -1,5 +1,7 @@
 package com.emmita.android.books.database;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -9,8 +11,24 @@ public class BookContract {
 
     }
 
+    /**Formando el URI
+    El nombre que se le asigna al content authority es recomendable que sea el nombre del paquete de la aplicación
+    ya que, es único en el proyecto*/
+    public static final String CONTENT_AUTHORITY = "com.emmita.android.books.database";
+    /**Concatenamos el content authority para formar la base del URI */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+ CONTENT_AUTHORITY);
+    /**Dirección donde se va a buscar le información de los libros*/
+    public static final String PATH_BOOKS = "books";
+
     //Tabla books
     public static final class BookEntry implements BaseColumns {
+
+        //Uri para accedes a la información desde el ContentProvider
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
 
         //Constante para el nombre de la tabla
         public final static String TABLE_NAME = "books";
